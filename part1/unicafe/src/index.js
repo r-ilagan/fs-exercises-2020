318369ad335d595fea5e34318c964c4266b7003d
Rach Ilagan <ilagan.rach@gmail.com>
2020-04-08 17:39:46 -0400

    Complete part 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Head = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.part[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.part[2]} {props.exercises[2]}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{' '}
+        {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -12,17 +47,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Head course={course} />
+      <Content
+        part={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   );
 };
