0d2ef52d56d33d505e69205073149284c3503742
Rach Ilagan <ilagan.rach@gmail.com>
2020-04-08 17:54:33 -0400

    Finish part 1.2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,14 +12,18 @@ const Head = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
       <p>
-        {props.part[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]} {props.exercises[2]}
+        {props.part} {props.exercises}
       </p>
     </div>
   );
