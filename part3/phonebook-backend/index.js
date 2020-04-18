ff99f7ee75aa4d946082e06dce0c368f2d525823
Rach Ilagan <ilagan.rach@gmail.com>
2020-04-17 22:53:19 -0400

    Finish 3.1-4

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people<br />${new Date()}</p>`
+  );
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
