04bbbee03017664fa425fa4ac7e16c55c38f277b
Rach Ilagan <ilagan.rach@gmail.com>
2020-04-23 21:09:37 -0400

    Fix mongoose error

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const url = process.env.MONGODB_URI;
+const url = String(process.env.MONGODB_URI);
 
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
