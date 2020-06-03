578988c6df4dfe7e9a10c9404ee21b789c9c8782
Rach Ilagan <Rach Ilagan>
2020-06-03 01:27:18 -0400

    Finish 5.7-5.8

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,41 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog, addLike }) => {
+  const [view, setView] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showInfo = view ? 'hide' : 'view';
+
+  const handleClick = () => {
+    setView(!view);
+  };
+
+  const displayInfo = { display: view ? '' : 'none' };
+
+  const updateLikes = async () => {
+    await addLike(blog);
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={handleClick}>{showInfo}</button>
+      <div style={displayInfo}>
+        {blog.url}
+        <br />
+        {`likes ${blog.likes}`} <button onClick={updateLikes}>like</button>
+        <br />
+        {blog.author}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
