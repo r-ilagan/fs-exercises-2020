a8718888dd628b2767c9196a36bfbb1606215c0d
Rach Ilagan <Rach Ilagan>
2020-06-02 21:09:56 -0400

    Lift state to BlogForm component

--- part5/src/components/BlogForm.js
+++ part5/src/components/BlogForm.js
@@ -1,38 +1,28 @@
 import React, { useState } from 'react';
 import Input from './Input';
-import blogService from '../services/blogs';
 
-const BlogForm = ({ blogs, setBlogs, notifyWith }) => {
+const BlogForm = ({ createBlog }) => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [url, setUrl] = useState('');
 
-  const handleCreate = async (event) => {
-    try {
-      event.preventDefault();
-      const blog = {
-        title,
-        author,
-        url,
-      };
-      const returnedBlog = await blogService.create(blog);
-      setBlogs(blogs.concat(returnedBlog));
-      setTitle('');
-      setAuthor('');
-      setUrl('');
-      notifyWith(
-        `A new blog ${returnedBlog.title} by ${returnedBlog.author} has beed added`
-      );
-    } catch (exception) {
-      notifyWith(exception.message, 'error');
-    }
+  const handleCreate = (event) => {
+    event.preventDefault();
+    createBlog({
+      title,
+      author,
+      url,
+    });
+    setTitle('');
+    setAuthor('');
+    setUrl('');
   };
 
   return (
     <form onSubmit={handleCreate}>
-      <Input name="title" handleChange={setTitle} />
-      <Input name="author" handleChange={setAuthor} />
-      <Input name="url" handleChange={setUrl} />
+      <Input name="title" value={title} handleChange={setTitle} />
+      <Input name="author" value={author} handleChange={setAuthor} />
+      <Input name="url" value={url} handleChange={setUrl} />
       <button type="submit">create</button>
     </form>
   );
